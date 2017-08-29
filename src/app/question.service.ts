b5925e26f9fc31cae06d7e99e82a44cf9cb8e481
Fernando Souza <nandosouzafilho@gmail.com>
2017-08-29 20:30:17 -0300

    Creates events to communicate with components

--- src/app/question.service.ts
+++ src/app/question.service.ts
@@ -1,17 +1,48 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Output, EventEmitter } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from "rxjs/Observable";
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/share';
+
+const POST_URL = 'http://localhost:8080/addQuestion/';
+const GET_URL = 'http://localhost:8080/questions';
 
 @Injectable()
 export class QuestionService {
+  private questions = [];
+  @Output() questionsUpdated = new EventEmitter();
 
   constructor(private http: Http) { }
 
+  private setQuestions(questions) {
+    this.questions = [...questions]
+    this.questionsUpdated.emit(this.questions)
+  }
+
   save(text: string): Observable<any> {
     if (!text) { return };
-    return this.http.post('http://localhost:8080/addQuestion/', { text })
-      .map(res => res.json());
+    let request = this.http.post(POST_URL, { text })
+      .map(res => res.json())
+      .share();
+
+    request.subscribe(data => {
+      this.setQuestions(data);
+    })
+    return request;
+  }
+
+  get(): Observable<any> {
+    let request = this.http.get(GET_URL)
+      .map(questions => questions.json())
+
+    request.subscribe(data => {
+      this.setQuestions(data);
+    })
+    return request;
+  }
+
+  getQuestions() {
+    return this.questions;
   }
 
 }
