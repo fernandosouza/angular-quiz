a8a7662a1bb15dd3d82e8bebf291b2f5804b9b38
Fernando Souza <nandosouzafilho@gmail.com>
2017-11-18 21:36:01 +0100

    Fix adding question

--- src/app/question-creator/question-creator.component.ts
+++ src/app/question-creator/question-creator.component.ts
@@ -1,44 +1,98 @@
-import { Component } from '@angular/core';
-import { QuestionService } from '../question.service';
+import { Component, Input, ViewChild, Optional, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, NgModel, NG_VALUE_ACCESSOR, NG_VALIDATORS, NG_ASYNC_VALIDATORS } from '@angular/forms';
+import { QuestionService, Question } from '../question.service';
 import { OptionService } from '../option.service';
+
+import { ElementBase } from '../app.element-base';
+import { Response } from '@angular/http';
+
+@Component({
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: NestedComponent,
+    multi: true,
+  }],
+  selector: 'app-nested',
+  template: `
+    <input
+      [(ngModel)]="value"
+      required
+      type="text"
+      #nested="ngModel"
+      placeholder="nested" />
+
+    <p *ngIf="nested.touched && !nested.valid">this is required</p>
+  `
+})
+export class NestedComponent extends ElementBase<string> {
+  @Input() public label: string;
+  @Input() public placeholder: string;
+  @ViewChild(NgModel) model: NgModel;
+
+  constructor(
+    @Optional() @Inject(NG_VALIDATORS) validators: Array<any>,
+    @Optional() @Inject(NG_ASYNC_VALIDATORS) asyncValidators: Array<any>,
+  ) {
+    super(validators, asyncValidators);
+  }
+}
 
 @Component({
   selector: 'app-question-creator',
   styleUrls: ['./question-creator.component.css'],
   template: `
   <article class="question">
-    <h5>
-      <input class="form-control" (input)="onInputQuestionName($event)" type="text" placeholder="Insert the question" name="question_text">
-    </h5>
-    <form>
+    <form #f="ngForm" (submit)="saveQuestion()">
+      <h5>
+        <input
+          required
+          class="form-control"
+          type="text"
+          placeholder="Insert the question"
+          name="question_text"
+          [(ngModel)]="question.text"
+        >
+      </h5>
       <ul class="list-group" *ngIf="options">
         <li class="list-group-item" *ngFor="let option of options; let i = index">
           <label class="sr-only" for="">Option</label>
           <input
+            [(ngModel)]="parent"
+            name="parent"
             class="form-control"
             placeholder="Type the option"
-            type="text" name="question_option[]"
-            (input)="onInputOptionText($event, i)">
+            type="text"
+            (input)="onInputOptionText($event, i)"
+          >
         </li>
       </ul>
-      <button class="btn btn-sm btn-block btn-primary" type="button" (click)="addOption()">Add new option</button>
+      <button
+        class="btn btn-sm btn-block btn-primary"
+        type="submit"
+      >
+        Add new option
+      </button>
     </form>
     <br>
-    <button class="btn btn-primary" (click)="saveQuestion()" type="button">Save</button>
+    <button
+      class="btn btn-primary"
+      type="submit"
+      [disabled]="f.invalid"
+    >
+      Save
+    </button>
     <br>
     <br>
-    {{ status }}
   </article>`
 })
 export class QuestionCreatorComponent {
-  private question = {
+  private question: Question = {
     text: ''
-  }
+  };
   private option = {
     text: ''
   }
   private options = [this.option]
-  private status: string;
   private questionId: number;
 
   constructor(
@@ -46,36 +100,33 @@ export class QuestionCreatorComponent {
     private optionService: OptionService
   ) { }
 
-  onInputQuestionName(event) {
-    const value = event.currentTarget.value;
-    this.question.text = value;
-  }
-
   addOption(): void {
     this.options = [...this.options, this.option]
   }
 
   saveQuestion(): void {
     if (!this.question.text) { return };
-    this.questionService.save(this.question.text)
-      .subscribe(data => {
-        if (data) {
-          this.status = 'Question saved'
-          this.questionId = data.insertId;
-          this.saveOptions(this.options, this.questionId);
+    this.questionService.save(this.question)
+      .subscribe((response: Response) => {
+        if (response) {
+          this.saveOptions(this.options, this.questionId)
+          this.resetQuestion()
         }
-        setTimeout(() => this.status = '', 2000);
-      });
+      })
   }
 
   onInputOptionText(event, index): void {
     this.options[index].text = event.currentTarget.value;
   }
 
+  resetQuestion() {
+    this.question.text = '';
+  }
+
   saveOptions(options: Array<object>, questionId: number) {
     this.optionService.save(this.options, questionId)
       .subscribe(data => {
-        console.log(data);
+        console.log(data)
       });
   }
 
